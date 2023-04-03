4ae0325ce0887480a7f07ad989f150c89f0dc08d
tazoakhalaia <darknemiroff@gmail.com>
2023-04-03 19:23:08 +0400

    delete invalidate and regenerate code

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -19,8 +19,6 @@ class SessionController extends Controller
 
     public function logout(Request $request) {
         Auth::logout();
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
         return redirect('login');
     }
 }
